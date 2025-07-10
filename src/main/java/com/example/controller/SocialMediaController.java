2b294028de5cf46176a703579d3510b72e1612ec
Angel Murillo <angel.f.murillo@gmail.com>
2025-07-10 19:17:55 +0000

    Register acct handler

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,12 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +14,35 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-public class SocialMediaController {
+@RestController
+ public class SocialMediaController {
+
+    @Autowired
+    private AccountService acctService;
+
+//    @Autowired
+//    private MessageService msgService;
+
+
+    //## 1: Our API should be able to process new User registrations.
+    @PostMapping("/register")
+    public ResponseEntity<?> registerAccount(@RequestBody Account acct){
+
+        String username = acct.getUsername();
+        String password = acct.getPassword();
+
+        if (username.isBlank() || password.length() < 4){
+            return ResponseEntity.badRequest().build();
+        }
+
+        Account createdAcct = acctService.registerUser(username, password);
+
+        if (createdAcct != null){ return ResponseEntity.ok(createdAcct);}
+        else return ResponseEntity.status(409).build();
+
+    }
+
+
+    
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,25 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+
+    @Autowired
+    private AccountRepository acctRepository;
+
+    public Account registerUser(String username, String password){
+
+        if (acctRepository.existsByUsername(username)) { return null;}
+
+        Account newAccount = new Account(username, password);
+        return acctRepository.save(newAccount);
+    } 
+
+
 }
